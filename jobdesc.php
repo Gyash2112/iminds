9656a5f95950c36ea536d9cb72a45f397b028631
vikhyatii <92918964+vikhyatii@users.noreply.github.com>
2023-02-14 11:38:40 +0530

    Add files via upload

---
+++ jobdesc.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="2">
+        <tr>
+        <td colspan="2">
+            <h3>LTIMindtree Recruitment 2023 BE/B.Tech–CSE/IT/Electronics/B.Sc |
+                 2018-2021 Batch | Bangalore
+            </h3>
+        </td>
+        </tr>
+        <tr>
+            <td> <h3>Important Dates</h3>
+                  <h4>Job Post:</h4> <p>10th February 2023</p>
+                  <h4>Last Date to Apply:</h4><p>20th February 2023</p>
+                  </td>
+            <td >  <h3>Job Location</h3>
+                    <p>Bangalore</p></td>
+        </tr>
+
+        <tr>
+            <td> <h2>Post Name</h2>
+                    <p>Infrastructure Monitoring Tools – IBM Netcool-Tivoli</p></td>
+            <td>  <h2>Eligibility</h2>
+                  <p>A Bachelor of Sciences/Engineering is preferred</p></td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <h2>How to apply?</h2>
+                <p>All interested and eligible candidates 
+                    can apply for this drive online by the following link as soon as possible.</p>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <h4>Apply Link:</h4>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
